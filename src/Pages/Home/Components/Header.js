9d9d87b4d985c2a0f9415fd8fdde24c6d7fe3ed9
Lux <lucasmmo2010@hotmail.com>
2021-03-01 16:03:31 -0300

    initial code

---
+++ src/Pages/Home/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header(){
+   return (
+    <header className = 'headrer'>
+    <div className = 'toolbar'>
+    <div className = ''>
+     <span>Conecta - Dev</span>
+    </div>
+    <div className = ''>
+     <button>Novo Post</button>
+     <span>img1</span>
+     <span>imag2</span>
+    </div>
+    </div>
+    </header>
+   )
+}
+
+export default Header;
